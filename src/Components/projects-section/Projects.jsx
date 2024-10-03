776d7e4b9ca794423a7a9860f37a830f8b0fd474
Maeva D <delruemaeva@orange.fr>
2024-10-04 00:20:17 +0200

    project section wip

--- src/Components/projects-section/Projects.jsx
+++ src/Components/projects-section/Projects.jsx
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 // styles
 import "../../common-rules.scss";
@@ -10,37 +10,96 @@ import "./projects.scss";
 import tripadvisor from "../../assets/pictures/tripadvisor.png";
 
 const Projects = () => {
-  //   const [showDescription, setShowDescription] = useState(false);
+  const [showDescription1, setShowDescription1] = useState(false);
+  const [showDescription2, setShowDescription2] = useState(false);
+  const [showDescription3, setShowDescription3] = useState(false);
+  const [showDescription4, setShowDescription4] = useState(false);
 
   return (
     <section className="projects">
       <div className="container sections">
         <h2>Projets</h2>
         <menu>
-          {/* <nav> */}
           <article>
             <img
               src={tripadvisor}
               alt="projet tripadvisor"
-              // onMouseEnter={() => setShowDescription(true)}
-              // onMouseLeave={() => setShowDescription(false)}
+              onMouseEnter={() => setShowDescription1(true)}
+              onMouseLeave={() => setShowDescription1(false)}
             />
-            {/* {showDescription && ( */}
-
-            <ul className="description">
-              <li>Responsive sur 3 breakpoints</li>
-              <li>Carousel de photos</li>
-              <li>Envoi automatisé de mail</li>
-            </ul>
-
-            {/* )} */}
+            {showDescription1 && (
+              <ul
+                className="description"
+                onMouseEnter={() => setShowDescription1(true)}
+                onMouseLeave={() => setShowDescription1(false)}
+              >
+                <h4>Trip Advisor</h4>
+                <h5>Projet front-end : HTML, CSS, Vanilla JS</h5>
+                <li>Responsive sur 3 breakpoints</li>
+                <li>Carousel de photos</li>
+                <li>Envoi automatisé de mail</li>
+              </ul>
+            )}
           </article>
-          <aside></aside>
-          {/* </nav> */}
-          {/* <nav>
-            <aside></aside>
-            <aside></aside>
-          </nav> */}
+          <article>
+            <img
+              src={tripadvisor}
+              alt="projet tripadvisor"
+              onMouseEnter={() => setShowDescription2(true)}
+              onMouseLeave={() => setShowDescription2(false)}
+            />
+            {showDescription2 && (
+              <ul
+                className="description"
+                onMouseEnter={() => setShowDescription2(true)}
+                onMouseLeave={() => setShowDescription2(false)}
+              >
+                <h4>Trip Advisor</h4>
+                <h5>Projet front-end : HTML, CSS, Vanilla JS</h5>
+                <li>Responsive sur 3 breakpoints</li>
+                <li>Carousel de photos</li>
+                <li>Envoi automatisé de mail</li>
+              </ul>
+            )}
+          </article>
+          <article>
+            <img
+              src={tripadvisor}
+              alt="projet tripadvisor"
+              onMouseEnter={() => setShowDescription3(true)}
+              onMouseLeave={() => setShowDescription3(false)}
+            />
+            {showDescription3 && (
+              <ul
+                className="description"
+                onMouseEnter={() => setShowDescription3(true)}
+                onMouseLeave={() => setShowDescription3(false)}
+              >
+                <li>Responsive sur 3 breakpoints</li>
+                <li>Carousel de photos</li>
+                <li>Envoi automatisé de mail</li>
+              </ul>
+            )}
+          </article>
+          <article>
+            <img
+              src={tripadvisor}
+              alt="projet tripadvisor"
+              onMouseEnter={() => setShowDescription4(true)}
+              onMouseLeave={() => setShowDescription4(false)}
+            />
+            {showDescription4 && (
+              <ul
+                className="description"
+                onMouseEnter={() => setShowDescription4(true)}
+                onMouseLeave={() => setShowDescription4(false)}
+              >
+                <li>Responsive sur 3 breakpoints</li>
+                <li>Carousel de photos</li>
+                <li>Envoi automatisé de mail</li>
+              </ul>
+            )}
+          </article>
         </menu>
       </div>
     </section>
