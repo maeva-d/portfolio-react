6323c13fd089ac5adb66f2a86f1491698904f087
Maeva D <delruemaeva@orange.fr>
2024-10-06 15:21:26 +0200

    added cart + refacto + style on scroll for header

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,17 +1,61 @@
+import { useState, useEffect } from "react";
+
 // Style
 import "./header.scss";
+import "../../common-rules.scss";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 96) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header">
-      <h1>Développeuse web et mobile</h1>
-      <nav>
-        <a href="">À propos de moi</a>
-        <a href="">Compétences</a>
-        <a href="">Projets</a>
-        <a href="">Contactez-moi</a>
-      </nav>
-      <ion-icon name="cart-outline"></ion-icon>
+    <header className={`header ${isScrolled && "transparent"}`}>
+      <div className="container">
+        <h1>Développeuse web et mobile</h1>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30px"
+            height="30px"
+            viewBox="0 0 30px 30px"
+          >
+            <g
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
+              color="currentColor"
+            >
+              <path d="m3.062 14.441l.365-2.142c.432-2.533.647-3.8 1.502-4.55C5.784 7 7.012 7 9.467 7h5.066c2.455 0 3.683 0 4.538.75s1.07 2.016 1.502 4.549l.365 2.142c.598 3.505.896 5.257-.023 6.408C19.995 22 18.295 22 14.898 22H9.102c-3.398 0-5.097 0-6.017-1.15c-.92-1.152-.62-2.904-.023-6.409" />
+              <path d="m7.5 9l.215-3.01a4.296 4.296 0 0 1 8.57 0L16.5 9" />
+            </g>
+          </svg>
+          <figure className="cart-item"></figure>
+        </div>
+        <nav>
+          <a href="">À propos de moi</a>
+          <a href="">Compétences</a>
+          <a href="">Projets</a>
+          <a href="">Contactez-moi</a>
+        </nav>
+      </div>
     </header>
   );
 };
